69860644595b569378fd3db703b29e18c03718e0
RuFi2k <filyk31@gmail.com>
2019-11-12 22:23:09 +0200

    Updates

---
+++ precache-manifest.c5bd0f7da92dd291f1ab737473d17077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f80d2828f0fbfa0d71ecfd79f1fef5f",
+    "url": "/graphics/index.html"
+  },
+  {
+    "revision": "bcd90191695a6cc4a5b8",
+    "url": "/graphics/static/css/main.3d51b9f6.chunk.css"
+  },
+  {
+    "revision": "25d1a71358c7ffc51ec8",
+    "url": "/graphics/static/js/2.3ab633b7.chunk.js"
+  },
+  {
+    "revision": "bcd90191695a6cc4a5b8",
+    "url": "/graphics/static/js/main.b3b3f6c8.chunk.js"
+  },
+  {
+    "revision": "d1d0677074c81d729c10",
+    "url": "/graphics/static/js/runtime-main.d3271ae4.js"
+  }
+]);
